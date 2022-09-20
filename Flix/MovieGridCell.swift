d6db24243e86698b4532c16498fefb587f7feeda
eluzung <luzungelaine@gmail.com>
2022-09-20 01:50:38 -0400

    Modified and Added Part 2 files. Updated README.md

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Elaine Luzung on 9/19/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
